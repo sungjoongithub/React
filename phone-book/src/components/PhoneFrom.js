e598bf365c2591e9ada80d640b729e9549043228
Park <sungjoonpark3@gmail.com>
2018-11-14 00:36:25 +0900

    Today Study

---
+++ phone-book/src/components/PhoneFrom.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class PhoneFrom extends Component {
+    state={
+        name:'',
+        phone:''
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        });
+    }
+    render() {
+        return (
+            <form>
+                <input 
+                placeholder="이름"
+                value={this.state.name}
+                onChange={this.handleChange}
+                name="name"
+                />
+                <input 
+                placeholder="전화번호"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                name="phone"
+                />
+            <div>
+                
+            </div>
+            </form>
+        );
+    }
+}
+
+export default PhoneFrom;
